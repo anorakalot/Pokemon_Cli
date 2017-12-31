b769c0d147b6494232a0c7e39921fd3530b74ca0
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-12-30 16:55:20 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,82 +1,100 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
-#include <string>
-//fstream is library for reading and writing files
-#include <fstream>
+#include <cstdlib>
+#include <ctime>
 #include <vector>
-
-//ZOMBIE INVENTORY CHECKER
-int main(){
-	/*
-	int a = 20;
-	int b = 10;
-	*/
-	//WRITING TO A FILE : INTS
-	std::cout  << "Ints"<< std::endl;
-	std::ofstream myfile;
-	myfile.open("ints.txt");
-
-
-
-	std::vector<int> myvect_1;
-
-	for (int x = 0; x < 11; ++x){
-		myvect_1.push_back(x);
-	}
-	for (int x = 0; x < myvect_1.size(); ++x){
-		myfile << myvect_1.at(x);
-		myfile << " ";
-	}
-	myfile.close();
-
-	//READING A FILE : INTS
-	std::ifstream myotherfile ("ints.txt");
-	int c;
-	std::vector<int> myvect_2;
-  	if (myotherfile.is_open())
-  		{
-
-    		while (myotherfile >> c)
-    			{
-						myvect_2.push_back(c);
-						std::cout << c<<std::endl;
-    			}
-
-    	myotherfile.close();
-  	}
-		/*
-		for (int x = 0; x < myvect_2.size(); ++x){
-			std::cout << myvect_2.at(x);
-		}
-		*/
-
-
-		//WRITING A FILE: STRING
-		std::vector<std::string> str_vect(2);
-
-		str_vect.at(0) = "I like apples.";
-		str_vect.at(1) = "Just kidding I despise apples";
-		std::ofstream str_file;
-		str_file.open("strings.txt");
-		for(int x = 0; x < str_vect.size(); ++x){
-			str_file << str_vect.at(x);
-			str_file << " ";
-		}
-		str_file.close();
-
-		//READING A FILE: STRING
-		std::string line;
-		std::vector<std::string> str_vect_2;
-		std::ifstream str_file_2("strings.txt");
-		if (str_file.is_open()){
-			while (str_file_2 >> line)
-			{
-				str_vect_2.push_back(line);
-			}
-			str_file_2.close();
-		}
-		for(int x = 0; x < str_vect_2.size(); ++x){
-			std::cout << str_vect_2.at(x)<<std::endl;
-		}
-//end of the program
-return 0;
+void make_board(int &board_size, int &location_determiner, int &width, int &length,std::vector<char> &locations,int&player_pos,char&location_of_player_pos){
+/*
+std::cout << "How wide do you want the gameboard to be?" << std::endl;
+std::cin >> width;
+std::cout << "How long do you want the gameboard to be?"<<std::endl;
+std::cin >> length;
+length = length + 1;
+*/
+width = 5;
+length = 5;
+board_size = length * width;
+locations.resize(board_size);
+for(int x = 0; x<locations.size(); ++x){
+  location_determiner = 0+(rand()%(10-0+1));
+  if (location_determiner>= 0 && location_determiner <=8){
+    locations.at(x) = 'O';
+  }
+  else{
+    locations.at(x) = 'Z';
+  }
 }
+//player position
+/*
+player_pos = 0+(rand()%(locations.size()-1-0+1));
+location_of_player_pos = locations.at(player_pos);
+locations.at(player_pos) = 'P';
+*/
+//end of make_board function
+}
+
+
+//diplay the board
+void display_board(std::vector<char> &locations,int &width){
+for(int x = 0; x < locations.size(); ++x){
+  if (x +1 % width==0 && x != 0){
+    std::cout << locations.at(x) << std::endl;
+  }
+  else{
+  std::cout << locations.at(x)<< "---";
+  }
+}
+}
+
+
+int main(){
+  //(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+srand(time(0));
+int board_size;
+int location_determiner;
+int width;
+int length;
+std::vector<char> locations(board_size);
+int player_pos;
+char location_of_player_pos;
+//width = 5;
+//board_size = 25;
+make_board(board_size,location_determiner,width,length,locations,player_pos,location_of_player_pos);
+display_board(locations,width);
+
+bool player_turn;
+
+player_turn = true;
+std::cout << std::endl;
+std::cout << "Make your move"<<std::endl;
+while(player_turn){
+
+if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+  player_pos ++;
+}
+
+}
+
+return 0;
+
+}
+
+
+
+
+//DEVELOPER NOTES
+/*
+Zombie game on a grid
+using keyboard to move around and make choices
+go to houses to collect supplies
+have a dice roll kill zombies
+goal is to survive as long as possible
+*/
+/*
+location types
+1.loot places (houses) (find loot at higher rates/zombies occur at lower rates POSSIBLY MAKE IT A HOMEBASE)
+2.open path (find loot at lower rates/zombies occur at higher rates)
+
+
+PUT IN MORE LOCATION TYPES LATER
+*/

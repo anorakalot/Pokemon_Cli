55234dca768c99b31c0bfeb2f00dfa2258a9660c
Richard "Dylan" McGee <sleepyanorak@gmail.com>
2017-12-31 16:09:39 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -21,29 +21,38 @@ for(int x = 0; x<locations.size(); ++x){
     locations.at(x) = 'O';
   }
   else{
-    locations.at(x) = 'Z';
+    locations.at(x) = 'H';
   }
 }
 //player position
-/*
-player_pos = 0+(rand()%(locations.size()-1-0+1));
+
+//player_pos = 0+(rand()%(locations.size()-1-0+1));
+player_pos = 20;
 location_of_player_pos = locations.at(player_pos);
 locations.at(player_pos) = 'P';
-*/
+
 //end of make_board function
 }
 
 
 //diplay the board
-void display_board(std::vector<char> &locations,int &width){
-for(int x = 0; x < locations.size(); ++x){
-  if (x +1 % width==0 && x != 0){
+void display_board(std::vector<char> &locations,int width){
+/*for(int x = 0; x < locations.size(); ++x){
+  if (x % width==0 && x != 0){
     std::cout << locations.at(x) << std::endl;
   }
   else{
   std::cout << locations.at(x)<< "---";
   }
-}
+}*/
+std::cout << std::endl<<std::endl;
+std::cout << locations.at(0)<< "---"<< locations.at(1)<< "---"<< locations.at(2)<< "---"<< locations.at(3)<< "---"<<locations.at(4)<<std::endl;
+std::cout << locations.at(5)<< "---"<< locations.at(6)<< "---"<< locations.at(7)<< "---"<< locations.at(8)<< "---"<<locations.at(9)<<std::endl;
+std::cout << locations.at(10)<< "---"<< locations.at(11)<< "---"<< locations.at(12)<< "---"<< locations.at(13)<< "---"<<locations.at(14)<<std::endl;
+std::cout << locations.at(15)<< "---"<< locations.at(16)<< "---"<< locations.at(17)<< "---"<< locations.at(18)<< "---"<<locations.at(19)<<std::endl;
+std::cout << locations.at(20)<< "---"<< locations.at(21)<< "---"<< locations.at(22)<< "---"<< locations.at(23)<< "---"<<locations.at(24)<<std::endl;
+
+
 }
 
 
@@ -70,7 +79,64 @@ std::cout << "Make your move"<<std::endl;
 while(player_turn){
 
 if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-  player_pos ++;
+  if (player_pos - 5<0){
+
+  }
+  else{
+    system("clear");
+    locations.at(player_pos) = location_of_player_pos;
+    player_pos -= 5;
+    location_of_player_pos = locations.at(player_pos);
+    locations.at(player_pos) = 'P';
+    display_board(locations,width);
+    player_turn = false;
+  }
+}
+
+else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+  if (player_pos + 5>24){
+
+  }
+  else{
+    system("clear");
+    locations.at(player_pos) = location_of_player_pos;
+    player_pos += 5;
+    location_of_player_pos = locations.at(player_pos);
+    locations.at(player_pos) = 'P';
+    display_board(locations,width);
+    player_turn = false;
+  }
+}
+
+else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+  if (player_pos + 5>24){
+
+  }
+  else{
+    system("clear");
+    locations.at(player_pos) = location_of_player_pos;
+    player_pos += 5;
+    location_of_player_pos = locations.at(player_pos);
+    locations.at(player_pos) = 'P';
+    display_board(locations,width);
+    player_turn = false;
+  }
+}
+
+
+else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+  if (player_pos + 5>24){
+
+  }
+  else{
+    system("clear");
+    locations.at(player_pos) = location_of_player_pos;
+    player_pos += 5;
+    location_of_player_pos = locations.at(player_pos);
+    locations.at(player_pos) = 'P';
+    display_board(locations,width);
+    player_turn = false;
+  }
 }
 
 }
